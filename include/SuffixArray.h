93ebde0342fba24fe4d482c305ab48a4c4712287
ThomasGibbonsIV <gibbonsiv.thomas@wustl.edu>
2022-07-23 12:25:09 -0500

    Doxygen updates

--- include/SuffixArray.h
+++ include/SuffixArray.h
@@ -12,15 +12,15 @@
 namespace CSE_584A
 {   
    /**
-   * \brief Computes the suffix array for string S.
+   * @brief Computes the suffix array for string S.
    *
    * Using the Manber Myers suffix array construction, the suffix array A is computed 
    * along with a rank array G.
    *
-   * \param S The string on which to compute the suffix array.
-   * \param A The suffix array of string S.
-   * \param G The rank array of string S.
-   * \param n The length of string S, suffix array A, and rank array G.
+   * @param[in] S: The string on which to compute the suffix array.
+   * @param[out] A: The suffix array of string S.
+   * @param[out] G: The rank array of string S.
+   * @param[in] n: The length of string S, suffix array A, and rank array G.
    */
    void ComputeSuffixArray(const char* S, int* A, int* G, int n);
 }
